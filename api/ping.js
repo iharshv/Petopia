143fa1173f9031e08f0dd12544665ada6fcd6ffe
Petopia <connectpetopia@gmail.com>
2026-02-03 01:20:23 +0530

    Add ping and update routing

---
+++ api/ping.js
@@ -0,0 +1,8 @@
+module.exports = async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.status(200).json({
+        status: 'ok',
+        message: 'Petopia API is alive!',
+        time: new Date().toISOString()
+    });
+};
